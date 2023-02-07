d8ce5240baf82f9e2ace43cfb1dbc20f1c8d4499
LAPTOP-DSAHINK9\jinyh <loseminho@gmail.com>
2023-02-07 18:44:42 +0900

    22.02.07 알고리즘 문제풀이

--- java/18108번/18108.java
+++ java/18108번/18108.java
@@ -2,14 +2,10 @@ import java.util.Scanner;
 
 public class Main{
 	public static void main(String[] args) {
-		
-		String addString = "??!";
-		
 		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
 		
-		System.out.println(s + addString);
-		
+		int year = sc.nextInt();
+		System.out.println(year - 543);
 	}
 	
 }
